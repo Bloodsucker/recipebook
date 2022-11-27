cf10bd95eda90dff1d2a0991caaa19230be2a9b5
Jose Cabo <jcabo@comscore.com>
2022-11-27 17:54:15 +0100

    Investigating custom querying.

--- src/main/java/com/jcabo/recipesapp/recipes/RecipesRepository.java
+++ src/main/java/com/jcabo/recipesapp/recipes/RecipesRepository.java
@@ -1,13 +1,25 @@
 package com.jcabo.recipesapp.recipes;
 
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @RepositoryRestResource(collectionResourceRel = "recipes", path = "recipes")
-interface RecipesRepository extends PagingAndSortingRepository<Recipe, Long> {
-    List<Recipe> findByVegetarian(boolean vegetarian);
+interface RecipesRepository extends JpaRepository<Recipe, Long>, RecipesRepositoryCustom {
+
+
+//    @Query("SELECT r FROM Recipe r WHERE r.vegetarian = :vegetarian")
+//    List<Recipe> filter(boolean vegetarian, String description);
 }
